b65663837171d45bf14e9fc603f234c98092cb5a
micah-olivas <micah5051olivas@gmail.com>
2023-10-16 19:30:17 -0700

    General development. Adding functionality

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 // script.js
+const text = "Quack-U-Weather Copyright 2023. A project by Oli the Younger.";
+let index = 0;
+
+function type() {
+    if (index < text.length) {
+        document.getElementById('credits').textContent += text.charAt(index);
+        index++;
+        setTimeout(type, 100);  // Adjust speed of typing
+    }
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let today = new Date().toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
+    type();  // Start typing animation
+    document.getElementById('title').innerText = `Click to predict daily outcomes for ${today}`;
+});
+
 async function getPrediction() {
     let response = await fetch('https://your-model-api-url.com/predict', {
         method: 'GET'

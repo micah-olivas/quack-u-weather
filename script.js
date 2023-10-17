7acda57ee1aa5775226fd29364897f8a2748ab58
micah-olivas <micah5051olivas@gmail.com>
2023-10-17 00:25:40 -0700

    General updates, adding prediction animations

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // script.js
-const text = "Quack-U-Weather Copyright 2023. A project by Oli the Younger.";
+const text = "Quack-U-Weather, Copyright 2023. A project by Oli the Younger.";
 let index = 0;
+let lookupTable = [];  // Assuming lookupTable is an array of objects. Populate it with your data.
 
 function type() {
     if (index < text.length) {
@@ -14,7 +15,7 @@ function randomBobbing() {
     const logoContainer = document.getElementById('logo-container');
     const maxX = 6;  // Maximum horizontal displacement (pixels)
     const maxY = 6;  // Maximum vertical displacement (pixels)
-    const maxDuration = 4;  // Maximum duration (seconds)
+    const maxDuration = 3;  // Maximum duration (seconds)
     
     // Generate random displacement and duration
     const x = Math.random() * maxX * (Math.random() < 0.4 ? 1 : -1);
@@ -36,24 +37,68 @@ document.addEventListener('DOMContentLoaded', (event) => {
         day: 'numeric'
     });
     type();  // Start typing animation
-    document.getElementById('title').innerText = `Click to predict daily outcomes for ${today}`;
 });
 
-async function getPrediction() {
-    let response = await fetch('https://your-model-api-url.com/predict', {
-        method: 'GET'
-    });
-    
-    let result = await response.json();
-    document.getElementById('result').innerText = 'Prediction: ' + result.prediction;
-
-    // Show the output div and populate it with the prediction results
+function getPrediction() {
+    const logoContainer = document.getElementById('logo-container');
+    const appBox = document.getElementById('app');
     const outputDiv = document.getElementById('output');
-    outputDiv.style.display = 'block'; // Show the div
-    outputDiv.textContent = 'Your prediction results go here'; // Replace this with your prediction results
+    const locationDiv = document.getElementById('location');
+    const loadingMessage = document.getElementById('loading-message');
+    
+    logoContainer.style.transform = 'translateY(-50px)';
+    
+    // Display the location and show the loading message
+    locationDiv.innerHTML = 'Wasco, CA \n<span style="font-family: Menlo, monospace;">(35.5941° N, 119.3409° W)</span>';
+    loadingMessage.style.display = 'block';
+    
+    // Transition to a specified max-height
+    appBox.style.transition = 'max-height 0.5s ease-in-out';
+    appBox.style.maxHeight = '500px';
+    
+    // Simulate fetching prediction
+    setTimeout(() => {
+        loadingMessage.style.display = 'none';
+        
+        // Manually set the predictionValue for testing
+        const predictionValue = 5;
+        
+        // Show the traffic lights once the prediction is received
+        document.getElementById('traffic-lights').style.visibility = 'visible';
+        
+        // Light up the traffic lights based on the predictionValue
+        for (let i = 1; i <= predictionValue; i++) {
+            document.getElementById(`light-${i}`).classList.add('active');
+        }
+        
+        // Display the message based on the predictionValue
+        const messageContainer = document.getElementById('message-container');
+        let message = '';
+        let color = '';  // new variable to hold the color value
+        switch (predictionValue) {
+            case '1': 
+                message = 'Poor Prediction';
+                color = 'red';  // color red for poor prediction
+                break;
+            case '2': case '3': 
+                message = 'Fair Prediction';
+                color = 'yellow';  // color yellow for fair prediction
+                break;
+            case '4': case '5': 
+                message = 'Good Prediction';
+                color = 'green';  // color green for good prediction
+                break;
+            default: 
+                message = 'No Prediction Available';
+                color = 'gray';  // color gray for no prediction available
+                break;
+        }
+        messageContainer.textContent = message;
+        messageContainer.style.color = color;  // set the color of the message text
+        messageContainer.classList.add('active');
+    }, 1000);
 }
 
 window.onload = function() {
-    /* ... (previous script) ... */
     randomBobbing();  // Start random bobbing
 };
